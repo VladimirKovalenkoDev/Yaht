6e5ec5290a7ead0e932422ce3c0848dc022e8805
vladimir <vovkazon@mail.ru>
2022-06-17 18:33:37 +0300

    Initial Commit

---
+++ Yaht/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yaht
+//
+//  Created by Владимир Коваленко on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
